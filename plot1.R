c36c4f31845189be50620338b56684b28e4da6af
AlonsoArrarte <alonso.arrarte@marketsoft.com.au>
2015-06-08 10:35:57 +1000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -9,25 +9,11 @@ febdata <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
 febdata$day <- weekdays(as.Date(febdata$Date, format="%d/%m/%y"))
 
 png(file="Plot1.png", width=480, height=480)
+
 hist(as.numeric(as.vector(febdata$Global_active_power)), 
      xlab = "Global Active Power (kilowatts)",
      main="Global Active Power",
      col = "Red"
      )
-dev.off
-
-<<<<<<< HEAD
-
-
-
-
-
-head(febdata)
-names(febdata)
-class(febdata$Global_active_power)
-febdata$Global_active_power
-head(febdata)
-head(as.numeric(febdata$Global_active_power))
-head(febdata$Global_active_power)
-=======
->>>>>>> 0353c679f00580152d93f9ecb92c50b517e90a0f
+     
+dev.off()
